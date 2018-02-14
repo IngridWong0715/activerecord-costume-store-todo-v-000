a21b4c4d6a071bb5423567e8d5a67e6cb31cf4f7
Ingrid Wong <yw340@georgetown.edu>
2018-02-14 06:29:50 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,12 +1,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
-      t.string :name,
-      t.string :location,
-      t.integer :costume_inventory,
-      t.integer :employees,
-      t.boolean :in_business,
-      t.timestamp :opening,
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :in_business
+      t.timestamp :opening
       t.timestamp :closing
 
     end
