bc1c03aef2593daf8da38f8a54c445849102c0d1
Ingrid Wong <yw340@georgetown.edu>
2018-02-14 06:27:17 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,6 +9,11 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
-  def change 
+  def change
+    create_table :costumes do |t|
+      t.string :name,
+      t.integer :price,
+      t.string :size,
+      t.string :url
   end
 end
