a6761556cf53d6a357d4aabf96b3e759496191e3
Ingrid Wong <yw340@georgetown.edu>
2018-02-14 06:39:25 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.datetime :opening
+      t.datetime :opening_date
       t.datetime :closing
       t.text :long_description
       t.timestamp
