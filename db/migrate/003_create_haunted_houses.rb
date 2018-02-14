b0515d0eea6300277f729d5da615e25a18f31e1b
Ingrid Wong <yw340@georgetown.edu>
2018-02-14 06:35:57 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,14 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :are_family_friendly
+      t.timestamp :opening
+      t.timestamp :closing
+      t.text :long_description
     end 
   end
 end
